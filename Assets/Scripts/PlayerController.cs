6d4ff2ed1198ce5b62ae8dba7efdceb817238831
Aidan <alh0125@auburn.edu>
2020-04-08 17:25:34 -0500

    grabbable objects

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 input;
+    public float speed = 1;
+    private Player player;
+    private CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = gameObject.GetComponent<CharacterController>();
+        player = gameObject.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 direction = player.hmdTransform.TransformDirection(input.axis.x, 0, input.axis.y);
+        characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - (9.81f * Vector3.up * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/SimpleGrab.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+public class SimpleGrab : MonoBehaviour
+{
+    private Interactable interactable;
+
+    void Start()
+    {
+        interactable = gameObject.GetComponent<Interactable>();
+    }
+
+    private void OnHandHoverBegin(Hand hand)
+    {
+        hand.ShowGrabHint();
+    }
+
+    private void OnHandHoverEnd(Hand hand)
+    {
+        hand.HideGrabHint();
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes grabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
+
+        if (interactable.attachedToHand == null && grabType != GrabTypes.None)
+        {
+            hand.AttachObject(gameObject, grabType);
+            hand.HoverLock(interactable);
+            hand.HideGrabHint();
+        }
+        else if (isGrabEnding)
+        {
+            hand.DetachObject(gameObject);
+            hand.HoverUnlock(interactable);
+        }
+    }
+}
