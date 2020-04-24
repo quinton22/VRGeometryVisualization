48111a69dafc00a5a79b4e9c2ff2853dacaa4781
Aidan <alh0125@auburn.edu>
2020-04-23 23:22:06 -0500

    added pen controller

---
+++ Assets/Scripts/PenInputController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class PenInputController : MonoBehaviour
+{
+    public SteamVR_Action_Boolean m_DrawAction;
+    private Interactable interactable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactable.attachedToHand != null)
+        {
+            SteamVR_Input_Sources source = interactable.attachedToHand.handType;
+            
+            if (m_DrawAction[source].stateDown) // trigger down
+            {
+                Draw();
+            }
+            else if (m_DrawAction[source].stateDown) // trigger hold
+            {
+                
+            }
+            else if (m_DrawAction[source].stateDown) // trigger up
+            {
+
+            }
+       
+        }
+    }
+
+    void Draw()
+    {
+        
+    }
+}
